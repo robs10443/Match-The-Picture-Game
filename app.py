58410b3d53eee289c6887f18cac9e3b36e6a0ab1
Robin Singh <robinsingh17151@aitpune.edu.in>
2020-07-03 14:20:54 +0530

    game_config upadted

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import pygame
 
-from pygame import display,event
+from pygame import display, event, image
 
 pygame.init()
 
 display.set_caption('Matching Pictures')
 
 screen = display.set_mode((512, 512))
+
+matched = image.load('Other_Assets/matched.png')
+screen.blit(matched, (0,0))
+display.flip()
 
 running = True
 
